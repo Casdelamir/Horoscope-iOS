d7b4ac3c4feb1f70ed6a33e482e7def0358070c0
Delamir <casdelamir@gmail.com>
2024-07-30 10:32:32 +0200

    Horoscope listed done

---
+++ Horoscope-iOS/View/TableViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  TableViewCell.swift
+//  Horoscope-iOS
+//
+//  Created by Mañanas on 30/7/24.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var icon: UIImageView!
+    
+    @IBOutlet weak var name: UILabel!
+    
+    @IBOutlet weak var date: UILabel!
+    
+}
--- Horoscope-iOS/ViewControllers/ListViewController.swift
+++ Horoscope-iOS/ViewControllers/ListViewController.swift
@@ -7,13 +7,30 @@
 
 import UIKit
 
-class ListViewController: UIViewController {
+class ListViewController: UIViewController, UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        tableView.dataSource = self
     }
-
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        HoroscopeProvider.horoscopes.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! TableViewCell
+         
+         // Configure the cell
+         let horoscope = HoroscopeProvider.horoscopes[indexPath.row]
+        cell.icon.image = horoscope.image
+        cell.name.text = horoscope.name
+        cell.date.text = horoscope.dateRange
+         
+         return cell
+    }
 }
 
